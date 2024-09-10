17358e0e0a3a16159927f03d140bc70de575d601
仁俊 何 <herenjun@sh-48-128.rnd.longcheer.net>
2024-09-10 17:34:34 +0800

    热页面

--- algorithm_string.cpp
+++ algorithm_string.cpp
@@ -1,10 +1,44 @@
-
+//热页面筛选
 #include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<algorithm>
 
 using namespace std;
-
 int main(){
-	
-	return 0;
+    int N;
+    cin >> N;
+    int counts=0;
+    map<int,int> count;
+    bool array[65536]={};
+    for(int i = 0; i < N; i++){
+        int x;
+        cin>>x;
+        if(array[x] ==0 ){
+            count[x] = 1;
+        }
+        else count[x]++;
+    }
+    int M;
+    cin >> M;
+    vector<pair<int,int>> vec;
+    for(auto iter = count.begin(); iter != count.end(); iter++){
+        vec.push_back((*iter));
+        if((*iter).second>=M){
+            counts++;
+        }
+    }
+    sort(vec.begin(),vec.end(),[](pair<int,int> x, pair<int,int> y){
+        return x.second > y.second;
+    });
+    //开始输出结果:
+    cout << counts << endl;
+    for(auto it: vec){
+        if(it.second >= M){
+            cout << it.first <<endl;
+        }
+        else break;
+    }
+    return 0;
 }
-
